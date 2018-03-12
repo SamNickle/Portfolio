8dac5a693c7fbd671178007f7ff62deb4416e867
Sam Nicholson <sam.nicholson@enquirymax.com>
2018-03-12 16:16:41 +0000

    Responsiveness completed, might need touching up. Started About me content

--- Portfolio/Controllers/AboutController.cs
+++ Portfolio/Controllers/AboutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,13 @@ namespace Portfolio.Controllers
     {
         // GET: /<controller>/
         public IActionResult Me()
+        {
+            var foo = Startup.Configuration.GetConnectionString("DefaultConnection");
+
+            return View();
+        }
+
+        public IActionResult Work()
         {
             return View();
         }
--- Portfolio/assets/js/site.js
+++ Portfolio/assets/js/site.js
@@ -1,21 +1,23 @@
 ﻿(function () {
     "use strict";
 
-    $('#Logo').click(function () {
-        window.location.replace("/");
-    });
-
-    $('.WorkImg').hover(function () {
-        $('.ImgOverlay').fadeIn();
-    });
-
-    // Sets height of absolute positined divs
+    // Makes first class of it's kind in an active state
     $('.SingleClient').first().addClass('active');
     $('.ClientLogo').first().addClass('active');
 
-    var ClientHeight = $('.SingleClient.active').outerHeight(true);
-    $('.ClientCarousel').height(ClientHeight);
+    // Creates reuseable function that sets the height of absolutely positioned elements
+    function ClientQuoteHeight() {
+        var ClientHeight = $('.SingleClient.active').outerHeight(true);
+        $('.ClientCarousel').height(ClientHeight);
+    }
 
+    // Sets height of absolutely positioned elements on load and on screen resize
+    ClientQuoteHeight();
+    $(window).resize(function () {
+        ClientQuoteHeight();
+    });
+
+    // Gives the clicked client a state of active and removes active state from other elements
     $('.ClientLogo').click(function () {
         var $this = $(this),
             $siblings = $this.parent().children(),
@@ -24,5 +26,21 @@
         $('.SingleClient').removeClass('active').eq(position).addClass('active');
         $siblings.removeClass('active');
         $this.addClass('active');
+        ClientQuoteHeight();
+    });
+
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
     });
 })();
