0c6b8d50c17a0f0ce13be385e6016fb377ffa4f3
Sam Nicholson <sam.nicholson@enquirymax.com>
2018-03-12 16:16:58 +0000

    Add files

---
+++ Portfolio/Models/WorkExperience.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class WorkExperience
+    {
+        public string Position { get; set; }
+        public string CompanyName { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string WorkDescription { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
